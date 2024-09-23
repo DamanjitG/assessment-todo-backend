0560f305dc0f10577122c1285962c99da403a31e
Daman Gill <daman.gill@uwaterloo.ca>
2024-09-22 23:09:53 -0400

    Implemented todo functionality

--- src/repositories/todo.js
+++ src/repositories/todo.js
@@ -6,7 +6,23 @@ export default (db) => {
         return await collection.insertOne(todo);
     }
 
+    async function findOne(todoID) {
+        return await collection.findOne({ todoID: todoID })
+    }
+
+    async function findAll(userID) {
+        return await collection.find({ userID: userID }).sort({ created: 1 }).toArray() // sort by creation date
+    }
+
+    async function updateOne(todoID, updates) {
+        // assumes "updates" are in the form {field: new value}
+        return await collection.updateOne({ todoID: todoID }, {$set:updates})
+    }
+
     return {
-        insertOne
+        insertOne,
+        findOne,
+        findAll,
+        updateOne
     };
 };
--- src/routers/todo.js
+++ src/routers/todo.js
@@ -23,7 +23,8 @@ export default ({todoRepository}) => {
                 ...req.body,
                 todoID,
                 userID: session.userID,
-                created
+                created,
+                done: false
             };
 
             if (validateTodo(newTodo)) {
@@ -38,6 +39,60 @@ export default ({todoRepository}) => {
             return res.status(500).send({error: "Todo creation failed."});
         }
     });
+
+    // get todo via ID
+    router.get('/:id', auth, async (req, res) => {
+        try {
+            let session = verifyToken(req.cookies['todox-session']);
+            const id = req.params.id;
+            const queriedTodo = await todoRepository.findOne(id);
+
+            // return todo if it exists
+            // we don't need to check if the user is correct because if the user doesn't own the todo, backend will return 403 error
+            if (queriedTodo) {
+                return res.status(200).send(queriedTodo);
+            } else {
+                return res.status(404).send({error: `Todo not found`});
+            }
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send({error: `GETting todo ${id} failed`});
+        }
+    })
+
+    // change a todo
+    router.patch('/:id', auth, async (req, res) => {
+        try {
+            let session = verifyToken(req.cookies['todox-session']);
+            const id = req.params.id;
+        
+            // check that todo exists
+            // we don't need to check if the user is correct because if the user doesn't own the todo, backend will return 403 error
+            const todo = await todoRepository.findOne(id);
+            if (!todo) {
+                return res.status(404).send({ error: 'Unable to update todo, todo not found'})
+            }
+        
+            const updatedTodo = await todoRepository.updateOne(id, req.body);
+        
+            return res.status(200).send(updatedTodo);
+          } catch (err) {
+            console.error(err);
+            return res.status(500).send({ error: 'Updating todo failed' });
+          }
+    })
+
+    // get all of user's todos
+    router.get('/', auth, async (req, res) => {
+        try {
+            let session = verifyToken(req.cookies['todox-session']);
+            const todos = await todoRepository.findAll(session.userID)
+            return res.status(200).send(todos)
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send({error: "Getting all todos failed"})
+        }
+    })
 
     return router;
 }
